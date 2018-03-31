3fda60d23dd58ca0b7cf44ec640e18a34140cf38
RSLi <robertsli@163.com>
2018-03-31 01:15:24 -0400

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import 'typeface-roboto';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-116657822-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
